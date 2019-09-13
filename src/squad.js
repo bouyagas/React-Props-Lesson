1b74aeae529233235d84ab82e74e87ec4c9767f4
mohamed gassama <bouyagassama@gmail.com>
2019-09-13 02:08:35 -0400

    semi done

---
+++ src/squad.js
@@ -0,0 +1,44 @@
+const squad = [
+  {
+    name: 'Brian Carela',
+    age: 26,
+    eyeColor: 'Black',
+  },
+  {
+    name: 'Elvis Garcia',
+    age: 25,
+    eyeColor: 'Hazel',
+  },
+
+  {
+    name: 'John Bolton',
+    age: 24,
+    eyeColor: 'Purple',
+  },
+
+  {
+    name: 'Ronaldo',
+    age: 23,
+    eyeColor: 'Yellow',
+  },
+
+  {
+    name: 'Messi',
+    age: 22,
+    eyeColor: 'Blue',
+  },
+
+  {
+    name: 'Lebrom',
+    age: 21,
+    eyeColor: 'Green',
+  },
+
+  {
+    name: 'James',
+    age: 20,
+    eyeColor: 'Brown',
+  },
+];
+
+export default squad;
